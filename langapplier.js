d9b7bfc054f2246d0000b83abd8773407442859b
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-10-09 15:05:51 +0200

    [1.0.0]

---
+++ langapplier.js
@@ -0,0 +1,82 @@
+"use-strict";
+
+const lap = new function() {
+	this.name = "LangApplier";
+	this.version = "1.0.0";
+	this.author = "Sv443";
+	this.desc = "Don't waste any time coding complex code in JS, just add a few attributes to your HTML elements instead!";
+}
+
+var blang;
+const dbg = false;
+
+document.addEventListener("DOMContentLoaded", function(){
+	blang = document.body.dataset.default_lang;
+	if(isempty(gebid("langapplier_disable_watermark"))) console.log("%c" + lap.name + " v" + lap.version + " - (c) Sv443 / Sven Fehler 2018 ( https://github.com/Sv443 ) - licensed under the MIT license", "background-color:#cccccc;color:black;padding:0.2em;font-size:1.1em;");
+	langapp.change(blang);
+});
+
+const langapp = new function() {
+	this.change = function(l) {
+		blang = document.body.dataset.lang;
+		document.body.dataset.lang = l;
+		if(dbg) clog('changing to "' + l + '"');
+		langapp.ctitle();
+		langapp.ccontent();
+		langapp.csrc();
+		
+		document.dispatchEvent(new Event("lang_change"));
+	}
+	this.ctitle = function() {
+		blang = document.body.dataset.lang;
+		var swel = gebcn("langapp");
+		for(var i = 0; i < swel.length; i++) {
+			if(isempty(swel[i].dataset.lang_title)){}
+			else {
+				var spl = swel[i].dataset.lang_title.split("|");
+				for(var j = 0; j < spl.length; j++) {
+					if(spl[j].includes(blang)) {
+						var spl2 = spl[j].split("°")[1];
+						swel[i].title=spl2;
+					}
+				}
+			}
+		}
+	}
+	this.ccontent = function() {
+		blang = document.body.dataset.lang;
+		var swel = gebcn("langapp");
+		for(var i = 0; i < swel.length; i++) {
+			if(isempty(swel[i].dataset.lang_content)){}
+			else {
+				var spl = swel[i].dataset.lang_content.split("|");
+				for(var j = 0; j < spl.length; j++) {
+					if(spl[j].includes(blang)) {
+						var spl2 = spl[j].split("°")[1];
+						swel[i].innerHTML=spl2;
+					}
+				}
+			}
+		}
+	}
+	this.csrc = function() {
+		blang = document.body.dataset.lang;
+		var swel = gebcn("langapp");
+		for(var i = 0; i < swel.length; i++) {
+			if(isempty(swel[i].dataset.lang_src)){}
+			else {
+				var spl = swel[i].dataset.lang_src.split("|");
+				for(var j = 0; j < spl.length; j++) {
+					if(spl[j].includes(blang)) {
+						var spl2 = spl[j].split("°")[1];
+						swel[i].src=spl2;
+					}
+				}
+			}
+		}
+	}
+}
+
+function gebid(id) {return document.getElementById(id);}
+function gebcn(classn) {return document.getElementsByClassName(classn);}
+function isempty(str) {if(str === null || str === undefined || str == "") return true; else return false;}
